799902e83519c3e3ed4267fb4932f4a72089bf1c
fncheng <dong862647669@gmail.com>
2021-10-14 17:23:23 +0800

    fix: example demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="home">
     <button @click="handleClick">handleClick</button>
-    <v-pie id="pie" :data="pieData" :option="pieOption"></v-pie>
+    <div class="wrapper">
+      <v-ring
+        id="pie"
+        :chartSetting="ringSetting"
+        :data="pieData"
+        :option="pieOption2"
+      ></v-ring>
+      <v-pie
+        id="pie1"
+        :data="pieData"
+        :option="pieOption1"
+        :legend="legend"
+      ></v-pie>
+    </div>
   </div>
 </template>
 
@@ -9,16 +22,38 @@
 import VPie from '../components/VPie.vue'
 import { pieOption } from './chartOption'
 import { onMounted, reactive } from '@vue/composition-api'
+import { cloneDeep } from 'lodash-es'
+import VRing from '../components/VRing.vue'
+let pieOption1 = cloneDeep(pieOption),
+  pieOption2 = cloneDeep(pieOption)
+pieOption1.series[0].radius = '50%'
+pieOption2.series[0].radius = ['40%', '50%']
 export default {
-  components: { VPie },
+  // eslint-disable-next-line
+  components: { VPie, VRing },
   name: 'Home',
+  data() {
+    // this.legend = {
+    //   icon: 'rect',
+    // }
+    return {
+      // legend: {
+      //   icon: 'rect',
+      // },
+    }
+  },
   setup() {
     let pieData = reactive([])
-
+    let ringSetting = {
+      limitShowNum: 2,
+    }
     function init() {
       let arr = [
         { value: 1048, name: '门急诊' },
         { value: 735, name: '住院' },
+        { value: 235, name: '123' },
+        { value: 535, name: '456' },
+        { value: 100, name: '789' },
       ]
       pieData.push(...arr)
     }
@@ -37,14 +72,29 @@ export default {
     onMounted(() => {
       init()
     })
-    return { pieData, pieOption, handleClick }
+    return {
+      pieData,
+      pieOption1,
+      pieOption2,
+      ringSetting,
+      handleClick,
+      legend: {
+        icon: 'pin',
+      },
+    }
   },
 }
 </script>
 
 <style lang="scss" scoped>
 .home {
-  width: 600px;
+  width: 800px;
   height: 600px;
+}
+.wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
 }
 </style>
